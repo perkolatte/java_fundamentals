79fcb43e876732580cf9e49cd4ebb96557db26b9
perkolatte <perkolatte@gmail.com>
2019-02-25 13:53:54 -0800

    all datatypes_operators exercises complete

--- src/datatypes_operators/labs/Exercise_03.java
+++ src/datatypes_operators/labs/Exercise_03.java
@@ -11,6 +11,17 @@ class ArithmeticOperators {
     public static void main(String[] args) {
 
         // write your code below
+        int four = 2 + 2;
+        int three = four - 1;
+        int two = (int) (0.67 * three);
+        int one = two / two;
+        int zero = one % one;
+
+        System.out.println("four = " + four);
+        System.out.println("three = " + three);
+        System.out.println("two = " + two);
+        System.out.println("one = " + one);
+        System.out.println("zero = " + zero);
 
     }
 
