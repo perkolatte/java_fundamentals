1178d6110b23d00c18c22d780a56988fd8239f5c
perkolatte <perkolatte@gmail.com>
2019-04-09 18:32:40 -0700

    completed packages lab

---
+++ src/labs_examples/packages/labs/package1/class1.java
@@ -0,0 +1,16 @@
+package labs_examples.packages.labs.package1;
+
+import labs_examples.packages.labs.package2.class2;
+
+public class class1 {
+
+    public static void main(String[] args) {
+
+//        class2.method3(); // inaccessible due to protected modifier
+        class2.method4();
+
+    }
+
+    public static void method2() {}
+
+}
---
+++ src/labs_examples/packages/labs/package2/class2.java
@@ -0,0 +1,8 @@
+package labs_examples.packages.labs.package2;
+
+public class class2 {
+
+    protected static void method3() {}
+    public static void method4() {}
+
+}
