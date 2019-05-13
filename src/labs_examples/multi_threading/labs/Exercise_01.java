0a2ed0f027bbc858cbd7cd65c3f3158bf13883b2
perkolatte <perkolatte@gmail.com>
2019-05-13 11:34:18 -0700

    Completed multi_threading Exercise_01

--- src/labs_examples/multi_threading/labs/Exercise_01.java
+++ src/labs_examples/multi_threading/labs/Exercise_01.java
@@ -8,4 +8,52 @@ package labs_examples.multi_threading.labs;
  *
  */
 
+class RunnableExample {
 
+    public static void main(String[] args) {
+
+        Runnable1 thread1 = new Runnable1("Thread #1");
+
+        Runnable1 runnable1 = new Runnable1();
+        Thread thread2 = new Thread(runnable1, "Thread #2");
+        thread2.start();
+
+        Thread thread3 = new Thread(new Runnable1(), "Thread #3");
+        thread3.start();
+
+        new Thread(new Runnable1(), "Thread #4").start();
+
+    }
+
+}
+
+class Runnable1 implements Runnable {
+
+    Thread thread;
+
+    public Runnable1() {
+    }
+
+    public Runnable1(String name) {
+
+        thread = new Thread(this, name);
+
+        thread.start();
+
+    }
+
+
+
+    @Override
+    public void run() {
+
+        try {
+
+            System.out.println(Thread.currentThread().getName() + " started!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+}
