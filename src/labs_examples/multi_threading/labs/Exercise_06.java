bec7e25a9c35d83ebe33ba89d4a7e39ea6860593
perkolatte <perkolatte@gmail.com>
2019-05-14 21:44:04 -0700

    Completed multi_threading Exercise_06

--- src/labs_examples/multi_threading/labs/Exercise_06.java
+++ src/labs_examples/multi_threading/labs/Exercise_06.java
@@ -7,3 +7,70 @@ package labs_examples.multi_threading.labs;
  *      print "1", then thread2 will print "2", then thread1 will print "3", then thread2 will print "4" and so on.
  */
 
+class Alternater {
+
+    public static void main(String[] args) {
+
+        AlternatingCounter odd = new AlternatingCounter("Odd");
+        AlternatingCounter even = new AlternatingCounter("Even");
+
+    }
+
+}
+
+class AlternatingCounter implements Runnable {
+
+    Thread thread;
+    static int count = 1;
+    static final Object monitor = new Object();
+
+    public AlternatingCounter(String name) {
+
+        thread = new Thread(this, name);
+
+        thread.start();
+
+    }
+
+    @Override
+    public void run() {
+
+        synchronized (monitor) {
+
+            if (count < 2) {
+                while (count <= 99) {
+
+                    try {
+
+                        thread.sleep(250);
+                        monitor.notifyAll();
+                        System.out.println(thread.getName() + ": " + count);
+                        count++;
+                        monitor.wait();
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            } else {
+                while (count <= 100) {
+
+                    try {
+
+                        thread.sleep(250);
+                        monitor.notifyAll();
+                        System.out.println(thread.getName() + ": " + count);
+                        count++;
+                        monitor.wait();
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+
+        }
+    }
+}
