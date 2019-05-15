2dfceda7988e520f45e67e65e361482cf74620da
perkolatte <perkolatte@gmail.com>
2019-05-14 20:56:20 -0700

    Completed multi_threading Exercise_05

--- src/labs_examples/multi_threading/labs/Exercise_05.java
+++ src/labs_examples/multi_threading/labs/Exercise_05.java
@@ -1,7 +1,67 @@
 package labs_examples.multi_threading.labs;
 
+import javax.management.Notification;
+
 /**
  * Multithreading Exercise 5:
  *
  *      Demonstrate the use of a wait() and notify()
  */
+
+class WaitNotifyExample {
+
+    public static void main(String[] args) {
+
+        WaitNotifyRunnable machine1 = new WaitNotifyRunnable("Machine 1");
+        WaitNotifyRunnable machine2 = new WaitNotifyRunnable("Machine 2");
+
+    }
+
+}
+
+class WaitNotifyRunnable implements Runnable {
+
+    Thread thread;
+    static boolean onlyThread = true;
+    static Object monitor = new Object();
+
+    public WaitNotifyRunnable(String machineName) {
+
+        thread = new Thread(this, machineName);
+
+        thread.start();
+
+    }
+
+    @Override
+    public void run() {
+
+        synchronized (monitor) {
+            System.out.println(thread.getName() + " booting up...");
+
+            if (onlyThread) {
+                onlyThread = false;
+
+                System.out.println(thread.getName() + " waiting...");
+
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println(thread.getName() + " received notification!");
+
+            } else {
+
+                try {
+                    thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(thread.getName() + " notifying...");
+                monitor.notifyAll();
+            }
+        }
+    }
+}
