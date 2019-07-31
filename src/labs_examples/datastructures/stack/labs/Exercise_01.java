2cd3de2f41b3cbc58b873c31ac8998c34bb8614b
perkolatte <perkolatte@gmail.com>
2019-07-30 17:23:28 -0700

    stacks labs Exercise_01 complete

--- src/labs_examples/datastructures/stack/labs/Exercise_01.java
+++ src/labs_examples/datastructures/stack/labs/Exercise_01.java
@@ -1,8 +1,38 @@
 package labs_examples.datastructures.stack.labs;
 
+import java.util.Stack;
+
 /**
  *      Demonstrate your mastery of Java's built-in Stack class by completing the following:
  *
  *      1) instantiate a new Stack
  *      2) demonstrate the use of EVERY method in the Stack class - there are 5
  */
+
+class Exercise_01 {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> plates = new Stack<>();
+
+        plates.push(1);
+        plates.push(2);
+        plates.push(3);
+        plates.push(4);
+        plates.push(5);
+        System.out.println("plates stack: " + plates.toString());
+
+        plates.pop();
+        System.out.println("plates stack: " + plates.toString());
+
+        System.out.println("top plate in stack: " + plates.peek());
+
+        System.out.println("number of plates in stack: " + plates.size());
+
+        System.out.println("out of plates: " + plates.empty());
+
+        System.out.println("index of plate 3 in stack: " + plates.search(3));
+
+    }
+
+}
